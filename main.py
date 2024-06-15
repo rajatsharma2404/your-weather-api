aab1511c4c2754178b98ee59cb73d077bf779e70
Rajat <rajatdoit18@gmail.com>
2024-06-15 16:49:59 +0530

    Added code to retrieve temperature for stations using API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask("Website")
 
@@ -8,7 +9,16 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    return "Hello"
+
+
+    filename = "data_small/" + "TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows = 20, parse_dates=['    DATE'])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
+    return {
+        "Station" : station,
+        "Date" : date,
+        "temp" : temperature
+    }
 
 
 if __name__ == "__main__":
